88fc9ba86c9042cd22334efc6c047d78ad51de24
DBreslin <breslin-d5@ulster.ac.uk>
2023-11-16 15:02:31 +0000

    Initial Page Creation

---
+++ app/src/main/java/com/example/pureflow_group_project/Splash.java
@@ -0,0 +1,14 @@
+package com.example.pureflow_group_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
