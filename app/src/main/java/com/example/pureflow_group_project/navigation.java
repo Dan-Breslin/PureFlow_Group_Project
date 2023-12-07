0b5fb27eb3d7760abc9396bc6af0dcf9e75e982f
Joel Varghese <Varghese-J2@ulster.ac.uk>
2023-12-07 15:01:56 +0000

    login and new nav page

--- app/src/main/java/com/example/pureflow_group_project/navigation.java
+++ app/src/main/java/com/example/pureflow_group_project/navigation.java
@@ -1,17 +1,14 @@
 package com.example.pureflow_group_project;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.Button;
 
-public class Login extends AppCompatActivity {
-    Button loginbtn;
+import android.os.Bundle;
+
+public class navigation extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        loginbtn = (Button) findViewById(R.id.login_btn);
+        setContentView(R.layout.activity_navigation);
     }
 }
